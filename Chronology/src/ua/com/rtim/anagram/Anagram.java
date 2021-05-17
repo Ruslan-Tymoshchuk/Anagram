8d718ca526b812044100fc27377c9b4363ce6ee9
R-Tim <R-Tim@AMD-Sapphire-4X-PC>
2021-05-17 19:44:52 +0300

    add stringbuilder to createanagram, removed unnecessary actions

--- Chronology/src/ua/com/rtim/anagram/Anagram.java
+++ Chronology/src/ua/com/rtim/anagram/Anagram.java
@@ -3,13 +3,13 @@ package ua.com.rtim.anagram;
 public class Anagram {
 
 	public String createAnagram(String text) {
-		String anagram = "";
-		String[] words = text.split(" ");
+		StringBuilder anagram = new StringBuilder();
+		final String space = " ";
+		String[] words = text.split(space);
 		for (String word : words) {
-			final String space = " ";
-			anagram += reverseWord(word) + space;
+			anagram.append(reverseWord(word)).append(space);
 		}
-		return anagram;
+		return anagram.toString();
 	}
 
 	private String reverseWord(String word) {
@@ -17,26 +17,19 @@ public class Anagram {
 		int leftIndex = 0;
 		int rightIndex = chars.length - 1;
 		while (leftIndex < rightIndex) {
-			if (Character.isLetter(chars[leftIndex]) &&
-					(Character.isLetter(chars[rightIndex]))) {
-				char array = chars[leftIndex];
-				chars[leftIndex] = chars[rightIndex];
-				chars[rightIndex] = array;
-				leftIndex++;
+			if (!Character.isLetter(chars[rightIndex])) {
 				rightIndex--;
-			} else if (Character.isLetter(chars[leftIndex]) && 
-					(!Character.isLetter(chars[rightIndex]))) {
-				rightIndex--;
-			} else if (!Character.isLetter(chars[leftIndex]) &&
-					(Character.isLetter(chars[rightIndex]))) {
+			} else if (!Character.isLetter(chars[leftIndex])) {
 				leftIndex++;
 			} else {
+				char symbol = chars[leftIndex];
+				chars[leftIndex] = chars[rightIndex];
+				chars[rightIndex] = symbol;
 				leftIndex++;
 				rightIndex--;
 			}
+
 		}
 		return String.valueOf(chars);
 	}
-
 }
-
