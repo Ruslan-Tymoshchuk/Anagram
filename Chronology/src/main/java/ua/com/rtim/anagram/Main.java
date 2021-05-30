9d0e30d2af381bd4e64672b8ecf1af1f335c898f
R-Tim <R-Tim@AMD-Sapphire-4X-PC>
2021-05-31 02:15:37 +0300

    update to maven project

---
+++ Chronology/src/main/java/ua/com/rtim/anagram/Main.java
@@ -0,0 +1,14 @@
+package ua.com.rtim.anagram;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.nextLine();
+		scanner.close();
+		Anagram anagram = new Anagram();
+		System.out.println("You'll get: " + anagram.createAnagram(text));
+	}
+}
